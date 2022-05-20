4df287ff3c68925e769e71d92de2aa5fa4b2cc12
Sam Glendenning <sam.glendenning@stfc.ac.uk>
2022-05-20 12:08:52 +0100

    Adding react query for MUI DataGrid

--- src/grid/grid.component.tsx
+++ src/grid/grid.component.tsx
@@ -1,46 +1,120 @@
 import React from 'react';
 import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import { usePeopleCount, usePeoplePaginated } from '../data/people';
+import { Person } from '../app.types';
 
 const Grid = React.memo((): React.ReactElement => {
+  const [page, setPage] = React.useState(0);
+  const [mydata, setMydata] = React.useState<rowData[]>([]);
+
+  const { data, isLoading } = usePeoplePaginated(page + 1);
+  const { data: count } = usePeopleCount();
+
+  type rowData = Person & {
+    id: number;
+  };
+
+  React.useEffect(() => {
+    if (data) {
+      setMydata(
+        data.map((person, i) => {
+          return {
+            ...person,
+            id: i + 1,
+          };
+        })
+      );
+    }
+  }, [data]);
+
+  const rows: GridRowsProp = React.useMemo(() => mydata ?? [], [mydata]);
+
   // All rows must have an ID. If not included as part of the row data, it can also be specified using getRowId prop on DataGrid
-  const rows: GridRowsProp = React.useMemo(
-    () => [
-      {
-        id: 1,
-        col1: 'Hello',
-        col2: 'World',
-      },
-      {
-        id: 2,
-        col1: 'react-table',
-        col2: 'rocks',
-      },
-      {
-        id: 3,
-        col1: 'whatever',
-        col2: 'you want',
-      },
-    ],
-    []
-  );
+  // const rows: GridRowsProp = React.useMemo(
+  //   () => [
+  //     {
+  //       id: 1,
+  //       col1: 'Hello',
+  //       col2: 'World',
+  //     },
+  //     {
+  //       id: 2,
+  //       col1: 'react-table',
+  //       col2: 'rocks',
+  //     },
+  //     {
+  //       id: 3,
+  //       col1: 'whatever',
+  //       col2: 'you want',
+  //     },
+  //   ],
+  //   []
+  // );
 
   const columns: GridColDef[] = React.useMemo(
     () => [
       {
-        field: 'col1', // the "key" in the data
-        headerName: 'Column 1',
+        headerName: 'Name',
+        field: 'name', // name is the "key" in the data
+        flex: 1,
       },
       {
-        field: 'col2',
-        headerName: 'Column 2',
+        headerName: 'Height',
+        field: 'height',
+      },
+      {
+        headerName: 'Mass',
+        field: 'mass',
+      },
+      {
+        headerName: 'Hair Color',
+        field: 'hair_color',
+      },
+      {
+        headerName: 'Skin Color',
+        field: 'skin_color',
+      },
+      {
+        headerName: 'Eye Color',
+        field: 'eye_color',
+      },
+      {
+        headerName: 'Birth Year',
+        field: 'birth_year',
+      },
+      {
+        headerName: 'Gender',
+        field: 'gender',
+      },
+      {
+        headerName: 'Created',
+        field: 'created',
+        flex: 1,
+      },
+      {
+        headerName: 'Edited',
+        field: 'edited',
+        flex: 1,
       },
     ],
     []
   );
 
   return (
-    <div style={{ height: 300, width: '100%' }}>
-      <DataGrid rows={rows} columns={columns} />
+    <div style={{ width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pagination
+        autoHeight
+        checkboxSelection
+        density="comfortable"
+        loading={isLoading}
+        onPageChange={(page) => setPage(page)}
+        paginationMode="server"
+        rowCount={count}
+        pageSize={10}
+      />
     </div>
   );
 });
